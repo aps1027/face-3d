764318281584cd5c76c23c8e846a6916980e77cf
aps1027 <pyaesonefpfsi@gmail.com>
2020-10-04 06:58:39 +0630

    [face3d] changed site name

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,13 +1,18 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap bg-teal-600 p-4 absolute w-screen">
+  <nav
+    class="flex items-center justify-between flex-wrap bg-teal-600 p-4 absolute w-screen"
+  >
     <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <span class="font-semibold md:text-2xl xl:text-2xl lg:text-2xl tracking-wide sm:text-xl">Room Interior Design Decoration</span>
+      <span
+        class="font-semibold md:text-2xl xl:text-2xl lg:text-2xl tracking-wide sm:text-xl"
+        >3D Room Interior Design Decoration</span
+      >
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Nav'
-}
+  name: "Nav",
+};
 </script>
