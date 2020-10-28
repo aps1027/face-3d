b94220836d8377ca394cd1a8c429504b037abf5e
aps1027 <pyaesonefpfsi@gmail.com>
2020-10-28 20:41:01 +0630

    [face3d #19] modified for deno server

--- server/controllers/RawItemConstroller.ts
+++ server/controllers/RawItemConstroller.ts
@@ -1,7 +1,7 @@
 import { RouterContext } from "../deps.ts";
 class RawItemController {
   async getRawItemList(ctx: RouterContext): Promise<void> {
-    ctx.response.body = "test";
+    ctx.response.body = "API tests";
   }
 }
 
--- server/main.ts
+++ server/main.ts
@@ -9,6 +9,18 @@ app.use(
   }),
 );
 
+app.addEventListener("listen", ({ hostname, port, secure }: any) => {
+  console.log(
+    `Listening on: ${secure ? "https://" : "http://"}${hostname ??
+      "localhost"}:${port}`,
+  );
+});
+
+app.addEventListener("error", (evt: any) => {
+  console.log(evt.error);
+});
+
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 5000 });
