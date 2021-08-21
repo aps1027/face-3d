38530a64f6762c909cc5d03a6fc652ce6846bd3a
aps1027 <pyaesonefpfsi@gmail.com>
2021-08-21 19:18:41 +0900

    [master] test env vairable

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,7 +8,7 @@
 import Nav from "./components/Nav.vue";
 
 console.log("Netlify DEPLOY_ID");
-console.log(process.env.DEPLOY_ID);
+console.log(process.env.VUE_APP_FIREBASE_PROJECT_ID);
 export default {
   name: "app",
   components: {
