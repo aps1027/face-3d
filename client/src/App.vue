301158c72d5077ec73c3bcc026743a6c23770dbc
aps1027 <pyaesonefpfsi@gmail.com>
2021-08-21 21:58:22 +0900

    [master] window.history.forward test

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,7 +10,7 @@ import Nav from "./components/Nav.vue";
 if (process.env.VUE_APP_DEPLOY_ID) {
   let deployId = localStorage.getItem("DEPLOY_ID");
   if (deployId !== process.env.VUE_APP_DEPLOY_ID) {
-    location.reload();
+    window.history.forward(1);
   }
   localStorage.setItem("DEPLOY_ID", process.env.VUE_APP_DEPLOY_ID);
 }
