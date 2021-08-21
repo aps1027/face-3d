be3bd90f9d1107da165ff4bbd4a1d1a2cb63f5e0
aps1027 <pyaesonefpfsi@gmail.com>
2021-08-21 22:43:24 +0900

    [master] fixed reloaded catching cache

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,13 +7,6 @@
 <script>
 import Nav from "./components/Nav.vue";
 
-if (process.env.VUE_APP_DEPLOY_ID) {
-  let deployId = localStorage.getItem("DEPLOY_ID");
-  if (deployId !== process.env.VUE_APP_DEPLOY_ID) {
-    window.history.forward(1);
-  }
-  localStorage.setItem("DEPLOY_ID", process.env.VUE_APP_DEPLOY_ID);
-}
 export default {
   name: "app",
   components: {
