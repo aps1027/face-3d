6e85ca8451895282c83f243052d6f4a25e57230d
aps1027 <pyaesonefpfsi@gmail.com>
2021-08-21 20:05:55 +0900

    [master] test deploy id

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,7 +8,7 @@
 import Nav from "./components/Nav.vue";
 
 console.log("Netlify DEPLOY_ID");
-console.log(process.env.VUE_APP_FIREBASE_PROJECT_ID);
+console.log(process.env.VUE_APP_DEPLOY_ID);
 export default {
   name: "app",
   components: {
